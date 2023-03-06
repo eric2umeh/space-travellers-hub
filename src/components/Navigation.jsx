58be9449fe10d637538c9f25b9dcc5de9c37382c
Eric <eric2umeh@yahoo.com>
2023-03-06 17:56:59 +0100

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,16 +3,17 @@ import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Navigation = () => (
-  <nav className="navbar">
+  <nav className="nav">
     <div className="links">
       <div className="logo">
         <img src={logo} className="logo-pic" alt="spacehub-logo" />
         <h1 className="logo-title">Space Travelers&apos Hub</h1>
       </div>
-      <ul>
-        <li><NavLink to="/">Rockets</NavLink></li>
-        <li><NavLink to="/missions">Missions</NavLink></li>
-        <li><NavLink to="/profile">My profile</NavLink></li>
+      <ul className="nav-links">
+        <li className="nav-link"><NavLink className={(linkData) => (linkData.isActive ? 'active-link' : '')}
+            to="/">Rockets</NavLink></li>
+        <li className="nav-link"><NavLink className={(linkData) => (linkData.isActive ? 'active-link' : '')} to="/missions">Missions</NavLink></li>
+        <li className="nav-link"><NavLink className={(linkData) => (linkData.isActive ? 'active-link' : '')} to="/profile">My profile</NavLink></li>
       </ul>
     </div>
   </nav>
