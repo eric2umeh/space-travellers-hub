2cb431d567d7283a9a53ed3b214624a82678904b
Eric <eric2umeh@yahoo.com>
2023-03-06 18:46:49 +0100

    Create Slice folder

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const Missions = () => {
-  return (
-    <div>Missions</div>
-  )
-}
+const Missions = () => (
+  <div>Missions</div>
+);
 
-export default Missions
+export default Missions;
