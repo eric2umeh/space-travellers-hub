a7c6cf0e3f674ec63dd0a6b60fed7e9bbd2682a9
Lugard Agu <agulugard@gmail.com>
2023-03-07 17:03:20 +0100

    update redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rockets from './rockets/rocketsSlice';
+import missions from './missions/missionsSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rockets.reducer,
+    missions: missions.reducer,
   },
 });
 
