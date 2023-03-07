49de4b34385f945ced89b3214629fe6442abb865
Eric <eric2umeh@yahoo.com>
2023-03-07 08:33:57 +0100

    Update the App.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
