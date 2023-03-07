18adab1c7e6f781f813c7d12105c6e2fb85cdaab
Eric <eric2umeh@yahoo.com>
2023-03-07 07:39:10 +0100

    Create rocketsSlice.js & Rockets.jsx

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchRocketsData } from '../redux/rockets/rocketsSlice';
 
 const Rockets = () => (
-  <div>Rockets</div>
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRocketsData());
+  }, [dispatch]);
+  return (
+    <div>
+      <h1>SpaceX rockets</h1>
+    </div>
+  );
 );
 
 export default Rockets;
