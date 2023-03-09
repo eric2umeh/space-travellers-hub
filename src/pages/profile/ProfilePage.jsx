09467431507b4d15c8635b03cb53fd734b5028f9
Eric <eric2umeh@yahoo.com>
2023-03-09 07:47:47 +0100

    Add reserved rockets to the ProfilePage.jsx'

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -5,16 +5,26 @@ import { useSelector } from 'react-redux';
 const Profile = () => {
   const rockets = useSelector((state) => state.rockets.rockets);
 
-  
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved !== false);
+
+  let rocketsContents;
+  if (reservedRockets.length === 0) {
+    return <p>No Rockets Reserved!</p>;
+  }
+  if (reservedRockets.length > 0) {
+    rocketsContents = reservedRockets.map((rocket) => (
+      <li key={rocket.rocket_id}>{rocket.rocket_name}</li>
+    ));
+  }
   return (
     <section>
       <div className="my_missions">
         <h3>My Missions</h3>
-        <ul className="profile_items">{missionsContent}</ul>
+        <ul className="profile_items">Missions Content</ul>
       </div>
       <div className="my_rockets">
         <h3>My Rockets</h3>
-        <ul className="profile_items">{rocketsContent}</ul>
+        <ul className="profile_items">{rocketsContents}</ul>
       </div>
     </section>
   );
